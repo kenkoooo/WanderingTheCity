7698ed58ed1e6432898ef94e0e96d1e299215738
kenkoooo <kenkou.n@gmail.com>
2016-10-26 17:12:19 +0900

    117:    338.99 118:    44.58 119:    658.02 120:    309.8 121:    159.48 122:    32.71 123:    49.91 124:    49.01 125:    383.34 126:    61.78 127:    168.86 128:    58.4 129:    170.89 130:    161.94 131:    56.84 132:    44.92 133:    897.34 134:    517.42 135:    414.84 136:    492.98 137:    61.74 138:    51.26 139:    34.97 140:    1176.44 141:    57.61 142:    1234.59 143:    428.33 144:    56.81 145:    57.25 146:    49.59 147:    353.76 148:    687.91 149:    177.86 150:    45.2 151:    53.31 152:    46.44 153:    58.4 154:    445.6 155:    167.29 156:    34.21 157:    270.64 158:    69.34 159:    44.12 160:    330.36 161:    214.5 162:    269.42 163:    46.17 164:    1478.83 165:    38.03 166:    28.08 167:    53.02 168:    344.64 169:    37.5 170:    43.36 171:    34.79 172:    896.33 173:    669.82 174:    33.83 175:    249.89 176:    52.51 177:    959.32 178:    59.96 179:    40.36 180:    291.66 181:    59.84 182:    224.78 183:    48.79 184:    232.82 185:    54.4 186:    67.48 187:    129.43 188:    149.47 189:    40.24 190:    37.35 191:    828.93 192:    254.85 193:    356.89 194:    35.0 195:    278.97 196:    42.55 197:    42.34 198:    48.22 199:    49.29 200:    553.39 201:    335.4 202:    325.25 203:    288.55 204:    31.8 205:    296.02 206:    55.36 207:    29.03 208:    29.94 209:    43.26 210:    838.64 211:    352.38 212:    141.52 213:    40.56 214:    149.98 215:    56.34 216:    180.24 Ave.    236.3640000000001

--- python/run.py
+++ python/run.py
@@ -1,13 +1,53 @@
 import subprocess
+import sys
+import time
+
+MAX_LEN = 40
+
+
+def get_progressbar_str(progress):
+    length = int(MAX_LEN * progress)
+    return ('[' + '=' * length +
+            ('>' if length < MAX_LEN else '') +
+            ' ' * (MAX_LEN - length) +
+            '] %.1f%%' % (progress * 100.))
 
 
 def main():
     min_seed = 117
     num = 100
+    score_dict = {}
+
+    start = time.time()
     for seed in range(min_seed, min_seed + num):
         output = subprocess.check_output(["java", "-cp", "out/production/WanderingTheCity",
-                                          "WanderingTheCityVis", "-seed", str(seed)])
-        print(output.decode("ascii"))
+                                          "WanderingTheCityVis", "-seed", str(seed)]).decode("ascii")
+        # 出力をパース
+        lines = output.split("\n")
+        score = float([line for line in lines if "Score = " in line][0].replace("Score = ", ""))
+        s = float([line for line in lines if "S = " in line][0].replace("S = ", ""))
+        score_dict[seed] = int(score / s / s * 10000) / 100.0
+
+        # 時間
+        elapsed_time = time.time() - start
+        sec_per_case = elapsed_time / (seed - min_seed + 1)
+        estimated_total = sec_per_case * num
+        remain_time = estimated_total - elapsed_time
+
+        # プログレスバーを表示
+        progress = 1.0 * (seed - min_seed + 1) / num
+        sys.stderr.write('\r\033[K' + get_progressbar_str(progress))
+        sys.stderr.write(('\tElapsed\t%.1f s' % elapsed_time))
+        sys.stderr.write(('\tRemain\t%.1f s' % remain_time))
+        sys.stderr.flush()
+    sys.stderr.write('\n')
+    sys.stderr.flush()
+
+    total = 0.0
+    for k, v in score_dict.items():
+        print("{seed}:\t{score}".format(seed=k, score=v))
+        total += v
+    print("Ave.\t{average}".format(average=(total / num)))
 
 
 if __name__ == '__main__':
